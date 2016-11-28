9c6024d277bf9c2f38fdd76cb3c4a64128a1bee5
johan <johan.gerdin@hotmail.com>
2016-11-28 15:48:37 +0100

    added test data for departments,programmes,hostedby,branches,students

--- assignment3/schema.sql
+++ assignment3/schema.sql
@@ -40,9 +40,9 @@ CREATE TABLE Branches (
 );
 
 CREATE TABLE Students (
-	NIN 			CHAR(11) NOT NULL PRIMARY KEY,
-	name 			TEXT 	NOT NULL,
-	loginID 		TEXT 	NOT NULL UNIQUE,
+	NIN 			CHAR(10) 	NOT NULL PRIMARY KEY,
+	name 			TEXT 		NOT NULL,
+	loginID 		TEXT 		NOT NULL UNIQUE,
 	branch 			TEXT,
 	programme 		TEXT,
 	FOREIGN KEY (branch,programme) REFERENCES Branches(name,programme)
@@ -147,7 +147,7 @@ CREATE TABLE Finished (
 --	FOR EACH ROW
 --	EXECUTE PROCEDURE updateBranchChildren();
 
-CREATE Procedure testData AS
+/*CREATE Procedure testData AS
 DECLARE iter INT = 0;
 GO
 BEGIN
@@ -155,49 +155,76 @@ BEGIN
 		INSERT INTO Departments (name,abbreviation) VALUES ('Department' + CAST(iter AS TEXT),'D' + CAST(iter AS TEXT));
 		SET iter = iter + 1;
 	END WHILE
-END
+END*/
 
 
 -- Test Data --
-INSERT INTO Departments (name,abbreviation) VALUES ('Computer Science','CS');
-INSERT INTO Departments (name,abbreviation) VALUES ('Physics','PS');
-INSERT INTO Departments (name,abbreviation) VALUES ('Chemistry','CH');
+INSERT INTO Departments (name,abbreviation) VALUES ('Department1','D1');
+INSERT INTO Departments (name,abbreviation) VALUES ('Department2','D2');
+INSERT INTO Departments (name,abbreviation) VALUES ('Department3','D3');
+INSERT INTO Departments (name,abbreviation) VALUES ('Department4','D4');
+INSERT INTO Departments (name,abbreviation) VALUES ('Department5','D5');
+INSERT INTO Departments (name,abbreviation) VALUES ('Department6','D6');
 
-INSERT INTO Programmes (name,abbreviation) VALUES ('Software Engineering','SE');
-INSERT INTO Programmes (name,abbreviation) VALUES ('Computer Engineering','CE');
+INSERT INTO Programmes (name,abbreviation) VALUES ('Programme1'	,'P1');
+INSERT INTO Programmes (name,abbreviation) VALUES ('Programme2'	,'P2');
+INSERT INTO Programmes (name,abbreviation) VALUES ('Programme3'	,'P3');
+INSERT INTO Programmes (name,abbreviation) VALUES ('Programme4'	,'P4');
+INSERT INTO Programmes (name,abbreviation) VALUES ('Programme5'	,'P5');
+INSERT INTO Programmes (name,abbreviation) VALUES ('Programme6'	,'P6');
+INSERT INTO Programmes (name,abbreviation) VALUES ('Programme7'	,'P7');
+INSERT INTO Programmes (name,abbreviation) VALUES ('Programme8'	,'P8');
+INSERT INTO Programmes (name,abbreviation) VALUES ('Programme9'	,'P9');
+INSERT INTO Programmes (name,abbreviation) VALUES ('Programme10','P10');
 
-INSERT INTO HostedBy (programme,department) VALUES ('Software Engineering','Computer Science');
-INSERT INTO HostedBy (programme,department) VALUES ('Computer Engineering','Computer Science');
+INSERT INTO HostedBy (programme,department) VALUES ('Programme1' ,'Department1');
+INSERT INTO HostedBy (programme,department) VALUES ('Programme1' ,'Department2');
+INSERT INTO HostedBy (programme,department) VALUES ('Programme2' ,'Department3');
+INSERT INTO HostedBy (programme,department) VALUES ('Programme2' ,'Department4');
+INSERT INTO HostedBy (programme,department) VALUES ('Programme3' ,'Department5');
+INSERT INTO HostedBy (programme,department) VALUES ('Programme3' ,'Department6');
+INSERT INTO HostedBy (programme,department) VALUES ('Programme4' ,'Department1');
+INSERT INTO HostedBy (programme,department) VALUES ('Programme4' ,'Department2');
+INSERT INTO HostedBy (programme,department) VALUES ('Programme5' ,'Department3');
+INSERT INTO HostedBy (programme,department) VALUES ('Programme5' ,'Department4');
+INSERT INTO HostedBy (programme,department) VALUES ('Programme6' ,'Department5');
+INSERT INTO HostedBy (programme,department) VALUES ('Programme6' ,'Department6');
+INSERT INTO HostedBy (programme,department) VALUES ('Programme7' ,'Department1');
+INSERT INTO HostedBy (programme,department) VALUES ('Programme7' ,'Department2');
+INSERT INTO HostedBy (programme,department) VALUES ('Programme8' ,'Department3');
+INSERT INTO HostedBy (programme,department) VALUES ('Programme8' ,'Department4');
+INSERT INTO HostedBy (programme,department) VALUES ('Programme9' ,'Department5');
+INSERT INTO HostedBy (programme,department) VALUES ('Programme9' ,'Department6');
+INSERT INTO HostedBy (programme,department) VALUES ('Programme10','Department1');
+INSERT INTO HostedBy (programme,department) VALUES ('Programme10','Department2');
 
-INSERT INTO Branches (name,programme) VALUES ('Interaction Design','Software Engineering');
-INSERT INTO Branches (name,programme) VALUES ('Software Engineering & Technology','Software Engineering');
-INSERT INTO Branches (name,programme) VALUES ('Algorithms, Languages & Logic','Computer Engineering');
+INSERT INTO Branches (name,programme) VALUES ('Branch1','Programme1');
+INSERT INTO Branches (name,programme) VALUES ('Branch2','Programme2');
+INSERT INTO Branches (name,programme) VALUES ('Branch3','Programme3');
+INSERT INTO Branches (name,programme) VALUES ('Branch4','Programme4');
+INSERT INTO Branches (name,programme) VALUES ('Branch5','Programme5');
+INSERT INTO Branches (name,programme) VALUES ('Branch6','Programme6');
+INSERT INTO Branches (name,programme) VALUES ('Branch1','Programme7');
+INSERT INTO Branches (name,programme) VALUES ('Branch2','Programme8');
+INSERT INTO Branches (name,programme) VALUES ('Branch3','Programme9');
+INSERT INTO Branches (name,programme) VALUES ('Branch4','Programme10');
+INSERT INTO Branches (name,programme) VALUES ('Branch5','Programme1');
+INSERT INTO Branches (name,programme) VALUES ('Branch6','Programme2');
 
-INSERT INTO Students (NIN,name,loginID,branch,programme) VALUES ('000001-0001','Johan Gerdin','gejohan','Interaction Design','Software Engineering');
-INSERT INTO Students (NIN,name,loginID,branch,programme) VALUES ('000002-0002','Lars Engstrom','englar','Software Engineering & Technology','Software Engineering');
-INSERT INTO Students (NIN,name,loginID,branch,programme) VALUES ('000003-0003','Lisa Hjalmarsson','hjalisa','Algorithms, Languages & Logic','Computer Engineering');
-
--- Display Tables --
---.print "========================================"
---.print "  Departments"
---.print "========================================"
---SELECT * FROM Departments;
---.print "========================================"
---.print "  Programmes"
---.print "========================================"
---SELECT * FROM Programmes;
---.print "========================================"
---.print "  HostedBy"
---.print "========================================"
---SELECT * FROM HostedBy;
---.print "========================================"
---.print "  Branches"
---.print "========================================"
---SELECT * FROM Branches;
---.print "========================================"
---.print "  Students"
---.print "========================================"
---SELECT * FROM Students;
+INSERT INTO Students (NIN,name,loginID,branch,programme) VALUES ('9008150001','name1','loginId1','Branch1','Programme0');
+INSERT INTO Students (NIN,name,loginID,branch,programme) VALUES ('9008150002','name2','loginId2','Branch2','Programme1');
+INSERT INTO Students (NIN,name,loginID,branch,programme) VALUES ('9008150003','name3','loginId3','Branch3','Programme2');
+INSERT INTO Students (NIN,name,loginID,branch,programme) VALUES ('9008150004','name4','loginId4','Branch4','Programme3');
+INSERT INTO Students (NIN,name,loginID,branch,programme) VALUES ('9008150005','name5','loginId5','Branch5','Programme4');
+INSERT INTO Students (NIN,name,loginID,branch,programme) VALUES ('9008150006','name6','loginId6','Branch6','Programme5');
+INSERT INTO Students (NIN,name,loginID,branch,programme) VALUES ('9008150007','name7','loginId7','Branch1','Programme6');
+INSERT INTO Students (NIN,name,loginID,branch,programme) VALUES ('9008150008','name8','loginId8','Branch2','Programme7');
+INSERT INTO Students (NIN,name,loginID,branch,programme) VALUES ('9008150009','name9','loginId9','Branch3','Programme8');
+INSERT INTO Students (NIN,name,loginID,branch,programme) VALUES ('9008150010','name10','loginId10','Branch4','Programme9');
+INSERT INTO Students (NIN,name,loginID,branch,programme) VALUES ('9008150011','name11','loginId11','Branch5','Programme10');
+INSERT INTO Students (NIN,name,loginID,branch,programme) VALUES ('9008150012','name12','loginId12','Branch6','Programme1');
+INSERT INTO Students (NIN,name,loginID,branch,programme) VALUES ('9008150013','name13','loginId13','Branch1','Programme2');
+INSERT INTO Students (NIN,name,loginID,branch,programme) VALUES ('9008150014','name14','loginId14','Branch2','Programme3');
 
 -- Views --
 DROP VIEW IF EXISTS Hosting;
@@ -250,11 +277,8 @@ CREATE VIEW StudentsAttendingProgramme AS
 --.print "========================================"
 --SELECT * FROM StudentsAttendingProgramme;
 
+SELECT * FROM Departments;
 SELECT * FROM Programmes;
-SELECT * FROM Branches;
-SELECT * FROM Students;
-DELETE FROM Programmes WHERE Programmes.name = 'Software Engineering';
---DELETE FROM Branches WHERE Branches.name = 'Interaction Design';
-SELECT * FROM Programmes;
+SELECT * FROM HostedBy;
 SELECT * FROM Branches;
 SELECT * FROM Students;
