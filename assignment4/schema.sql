bbbd00f4e0421376d680027b77766a980374ad33
AemW <andreas.wahlstrom@hotmail.com>
2016-12-06 08:22:35 +0100

    added folder for ass4 and added files from ass3

---
+++ assignment4/schema.sql
@@ -0,0 +1,306 @@
+DROP VIEW IF EXISTS HostingDepartmentProgramme;
+DROP VIEW IF EXISTS Hosting;
+DROP VIEW IF EXISTS NotHostingDepartmentProgramme;
+DROP VIEW IF EXISTS NotHosting;
+DROP VIEW IF EXISTS StudentsAttendingProgramme;
+
+DROP VIEW IF EXISTS PathToGraduation;
+DROP VIEW IF EXISTS UnreadMandatory;
+DROP VIEW IF EXISTS PassedCourses;
+DROP VIEW IF EXISTS Registrations;
+DROP VIEW IF EXISTS FinishedCourses;
+DROP VIEW IF EXISTS StudentsFollowing;
+
+-- in backwards order since psql complains
+-- about tables depending on other tables
+DROP TABLE IF EXISTS Finished;
+DROP TABLE IF EXISTS Registered;
+DROP TABLE IF EXISTS Recommended;
+DROP TABLE IF EXISTS BranchMandatory;
+DROP TABLE IF EXISTS ProgrammeMandatory;
+DROP TABLE IF EXISTS Prerequisite;
+DROP TABLE IF EXISTS WaitingOn;
+DROP TABLE IF EXISTS HasClass;
+DROP TABLE IF EXISTS Classifications;
+DROP TABLE IF EXISTS LimitedCourses;
+DROP TABLE IF EXISTS ChosenBranch;
+DROP TABLE IF EXISTS Students;
+DROP TABLE IF EXISTS Courses;
+DROP TABLE IF EXISTS Branches;
+DROP TABLE IF EXISTS HostedBy;
+DROP TABLE IF EXISTS Programmes;
+DROP TABLE IF EXISTS Departments;
+
+CREATE TABLE Departments (
+	name			TEXT	NOT NULL PRIMARY KEY,
+	abbreviation	TEXT	NOT NULL UNIQUE
+);
+
+CREATE TABLE Programmes (
+	name			TEXT	NOT NULL PRIMARY KEY,
+	abbreviation	TEXT	NOT	NULL
+);
+
+CREATE TABLE HostedBy (
+	programme 		TEXT 	NOT NULL,
+	department 		TEXT 	NOT NULL REFERENCES Departments(name),
+	FOREIGN KEY (programme) REFERENCES Programmes(name),
+	PRIMARY KEY (programme,department)
+);
+
+CREATE TABLE Branches (
+	name			TEXT	NOT NULL,
+	programme 		TEXT 	NOT NULL,
+	FOREIGN KEY (programme) REFERENCES Programmes(name),
+	PRIMARY KEY (name,programme)
+);
+
+CREATE TABLE Students (
+	NIN 			CHAR(10) 	NOT NULL PRIMARY KEY,
+	name 			TEXT 		NOT NULL,
+	loginID 		TEXT 		NOT NULL UNIQUE,
+	programme 		TEXT		NOT NULL REFERENCES Programmes(name)
+);
+
+CREATE TABLE ChosenBranch (
+	student 		CHAR(10) 	NOT NULL REFERENCES Students(NIN) PRIMARY KEY,
+	branch 			TEXT 		NOT NULL,
+	programme 		TEXT 		NOT NULL
+);
+
+-- This trigger function does:
+-- - check that a chosen branch actually belong to the program the student
+--   is enrolled in.
+CREATE OR REPLACE FUNCTION checkBranchInProgramme() 
+RETURNS TRIGGER AS $$
+BEGIN
+	IF NEW.branch IN (SELECT Branches.name FROM Branches WHERE Branches.programme = NEW.programme) THEN
+		RETURN NEW;
+	ELSE
+		RAISE 'Branch -> % not in programme -> %', NEW.branch,New.programme;
+	END IF;
+	RETURN NEW;
+END
+$$ LANGUAGE 'plpgsql';
+
+CREATE TRIGGER branchInProgramme BEFORE INSERT ON ChosenBranch
+	FOR EACH ROW EXECUTE PROCEDURE checkBranchInProgramme();
+
+CREATE TABLE Courses (
+	code		CHAR(6) NOT NULL PRIMARY KEY,
+	name		TEXT NOT NULL,
+	credits		REAL NOT NULL,
+	department	TEXT NOT NULL REFERENCES Departments(name)
+);
+
+CREATE TABLE LimitedCourses (
+	code			CHAR(6) NOT NULL REFERENCES Courses(code) PRIMARY KEY,
+	studentLimit	INT		NOT NULL CHECK(studentLimit >= 0)
+);
+
+CREATE TABLE Classifications (
+	class	TEXT NOT NULL PRIMARY KEY
+);
+
+CREATE TABLE HasClass (
+	course	CHAR(6) NOT NULL REFERENCES Courses(code),
+	class	TEXT 	NOT NULL REFERENCES Classifications(class),
+	PRIMARY KEY (course, class)
+);
+
+CREATE TABLE WaitingOn (
+	course	CHAR(6) 	NOT NULL REFERENCES LimitedCourses(code),
+	student	CHAR(11) 	NOT NULL REFERENCES Students(NIN),
+	date	TEXT 		NOT NULL UNIQUE,
+	PRIMARY KEY (course, student)
+);
+
+CREATE TABLE Prerequisite (
+	prerequisite	CHAR(6) NOT NULL REFERENCES Courses(code),
+	toCourse		CHAR(6) NOT NULL REFERENCES Courses(code),
+	PRIMARY KEY (prerequisite, toCourse),
+	CHECK(NOT (prerequisite = toCourse))
+	-- REMEMBER assertion for (c1 to c2 to c1)... 
+);
+
+-- This trigger function does:
+-- - check if inserting the new prerequisite create a cycle
+-- - - If it does then we don't insert it
+-- - - otherwise just insert
+CREATE OR REPLACE FUNCTION checkCycle()
+RETURNS TRIGGER AS $$
+DECLARE
+    arr bpchar[];
+BEGIN
+    -- copy table and insert new prerequisite into copy
+    CREATE TEMP TABLE prereq AS SELECT * FROM Prerequisite;
+    INSERT INTO prereq VALUES (NEW.prerequisite, NEW.toCourse);
+
+    -- look for prerequisite depenencies
+    IF EXISTS (
+       WITH RECURSIVE prev AS (
+            SELECT p.prerequisite, 
+                1 AS depth, 
+                arr || p.prerequisite  as seen, 
+                false as cycle
+            FROM prereq p
+            WHERE p.prerequisite = NEW.toCourse
+                UNION ALL
+                    SELECT p.prerequisite, 
+                        prev.depth + 1, 
+                        seen || p.prerequisite as seen, 
+                        p.prerequisite = any(seen) as cycle
+                    FROM prev
+                    INNER JOIN prereq p on prev.prerequisite = p.toCourse
+                    AND prev.cycle = false
+        )
+        SELECT 1 
+        FROM prev
+        WHERE cycle
+        LIMIT 1
+        )
+    THEN
+        -- Detected cycle, abort
+        DROP TABLE prereq;
+        RAISE 'Cycle detected: % -> %', NEW.prerequisite, NEW.toCourse;
+    ELSE
+        -- No cycle, insert new prerequisite
+        DROP TABLE prereq;
+        RETURN NEW;
+    END IF; 
+
+END
+$$ LANGUAGE 'plpgsql';
+
+CREATE TRIGGER cycle BEFORE INSERT ON Prerequisite
+	FOR EACH ROW EXECUTE PROCEDURE checkCycle();
+
+CREATE TABLE ProgrammeMandatory (
+	programme	TEXT 	NOT NULL REFERENCES Programmes(name),
+	course		CHAR(6) NOT NULL REFERENCES Courses(code),
+	PRIMARY KEY (programme, course)
+);
+
+CREATE TABLE BranchMandatory (
+	branch		TEXT 	NOT NULL,
+	programme	TEXT 	NOT NULL,
+	course		CHAR(6) NOT NULL REFERENCES Courses(code),
+	FOREIGN KEY (branch,programme) REFERENCES Branches(name,programme),
+	PRIMARY KEY (programme, branch, course)
+);
+
+CREATE TABLE Recommended (
+	branch		TEXT 	NOT NULL,
+	programme	TEXT 	NOT NULL,
+	course		CHAR(6) NOT NULL REFERENCES Courses(code),
+	FOREIGN KEY (branch,programme) REFERENCES Branches(name,programme),
+	PRIMARY KEY (programme, branch, course)
+);
+
+CREATE TABLE Registered (
+	student	CHAR(11) 	NOT NULL REFERENCES Students(NIN),
+	course	CHAR(6) 	NOT NULL REFERENCES Courses(code),
+	PRIMARY KEY (student, course)
+);
+
+-- This trigger function does:
+-- - Check that a student fulfilles the prerequisites for a course s/he wants to register on
+-- - Check if the course is limited AND
+-- - - that there are spots left and if so register the student.
+--     Also decriments the number of free spots on the course by one.
+-- - - places the student on the waiting list for that course if
+--     there are no spots left
+CREATE OR REPLACE FUNCTION hasClearedPrerequisites() 
+RETURNS TRIGGER AS $$
+DECLARE
+    arr bpchar[];
+BEGIN
+	CREATE TEMP TABLE prereq AS 
+        SELECT * FROM Prerequisite 
+        WHERE NEW.course = Prerequisite.toCourse; --all prereq to intresting course
+    CREATE TEMP TABLE fin AS 
+        SELECT course FROM Finished 
+        WHERE NEW.student = Finished.student; -- student's finished courses
+
+    -- look up the requirements. 
+    -- TODO Actually, recursive is overkill. Should just need to check
+    -- requirements in one step...
+    IF EXISTS (
+      WITH RECURSIVE prev AS (
+                SELECT p.prerequisite, 
+                    arr || NEW.course || p.prerequisite AS seen, 
+                    p.prerequisite IN (SELECT * FROM fin) AS qualified
+                FROM Prerequisite p
+                WHERE p.toCourse = NEW.course
+                    UNION ALL
+                        SELECT p.prerequisite, 
+                            seen || p.prerequisite As seen, 
+                            prev.prerequisite IN (SELECT * FROM fin) AS qualified
+                        FROM prev
+                        INNER JOIN Prerequisite p ON prev.prerequisite = p.toCourse
+        )
+        SELECT 1 
+        FROM prev
+        WHERE NOT qualified
+        LIMIT 1
+    ) 
+    THEN 
+        -- student doesn't fulfill the requirements for the course
+        DROP TABLE IF EXISTS fin;
+        DROP TABLE IF EXISTS prereq;
+        RAISE 'Student % have not taken all prerequisite courses for course %', 
+                NEW.student, NEW.course;
+    ELSE
+        DROP TABLE IF EXISTS fin;
+        DROP TABLE IF EXISTS prereq;
+        IF EXISTS (SELECT code FROM LimitedCourses WHERE LimitedCourses.code = NEW.course) THEN
+            IF (SELECT studentLimit FROM LimitedCourses WHERE LimitedCourses.code = NEW.course) > 0 THEN
+                -- decriment number of free spots by 1
+                UPDATE LimitedCourses SET studentLimit = studentLimit - 1
+                WHERE LimitedCourses.code = NEW.course;
+                -- register
+                RETURN NEW;
+            ELSE
+                -- No spots left on course, place in waiting list
+                RAISE NOTICE 'Course "%" is full, placing student % on waiting list', NEW.course, NEW.student;
+                INSERT INTO WaitingOn VALUES (NEW.course, NEW.student, CURRENT_TIME);
+                RETURN NULL;
+            END IF;
+        ELSE
+            -- register
+            RETURN NEW;
+        END IF;
+    END IF;
+END
+$$ LANGUAGE 'plpgsql';
+
+CREATE TRIGGER check_qualifications BEFORE INSERT ON Registered
+	FOR EACH ROW EXECUTE PROCEDURE hasClearedPrerequisites();
+
+-- This trigger function increases the course limit by one after a student 
+-- has been deleted, but only if the course was a limitedcourse
+CREATE OR REPLACE FUNCTION correct_limit() 
+RETURNS TRIGGER AS $$
+DECLARE
+    _waitingStudent CHAR(10);
+BEGIN
+    IF EXISTS (SELECT code FROM LimitedCourses WHERE LimitedCourses.code = OLD.course) THEN
+        -- increment number of free spots by 1
+        UPDATE LimitedCourses SET studentLimit = studentLimit + 1
+        WHERE LimitedCourses.code = OLD.course;
+        -- TODO check waitinglist and register student on course
+        RETURN OLD; 
+    END IF;
+END
+$$ LANGUAGE 'plpgsql';
+
+CREATE TRIGGER increse_limit AFTER DELETE ON Registered
+    FOR EACH ROW EXECUTE PROCEDURE correct_limit();
+
+
+CREATE TABLE Finished (
+	student	CHAR(11) 	NOT NULL REFERENCES Students(NIN),
+	course	CHAR(6) 	NOT NULL REFERENCES Courses(code),
+	grade	CHAR(1) 	NOT NULL CHECK(grade IN ('U', '3', '4', '5')),
+	PRIMARY KEY (student, course)
+);
---
+++ assignment4/view.sql
@@ -0,0 +1,140 @@
+-- Views --
+/*DROP VIEW IF EXISTS HostingDepartmentProgramme;
+DROP VIEW IF EXISTS Hosting;
+DROP VIEW IF EXISTS NotHostingDepartmentProgramme;
+DROP VIEW IF EXISTS NotHosting;
+
+CREATE VIEW Hosting AS
+	SELECT * FROM Departments 
+		WHERE Departments.name IN
+			(SELECT department FROM HostedBy);
+
+CREATE VIEW HostingDepartmentProgramme AS
+	SELECT 	Hosting.name AS department,
+			Hosting.abbreviation AS deptAbbreviation,
+			Programmes.name AS programme,
+			Programmes.abbreviation AS progAbbreviation
+		FROM Hosting NATURAL JOIN HostedBy 
+			JOIN Programmes ON HostedBy.programme = Programmes.name;
+
+CREATE VIEW NotHosting AS
+	SELECT * FROM Departments 
+		WHERE Departments.name NOT IN
+			(SELECT department FROM HostedBy);
+
+CREATE VIEW NotHostingDepartmentProgramme AS
+	SELECT 	NotHosting.name AS department,
+			NotHosting.abbreviation AS deptAbbreviation,
+			Programmes.name AS programme,
+			Programmes.abbreviation AS progAbbreviation
+		FROM NotHosting NATURAL JOIN HostedBy 
+			JOIN Programmes ON HostedBy.programme = Programmes.name;
+
+DROP VIEW IF EXISTS StudentsAttendingProgramme;
+CREATE VIEW StudentsAttendingProgramme AS
+	SELECT Students.name AS Student,Programmes.name AS Programme FROM
+		Students JOIN Programmes ON Students.programme = Programmes.name;
+		*/
+-- StudentsFollowing
+-- For all students, their basic information (name etc.), and the programme and branch (if any) they are following.
+DROP VIEW IF EXISTS StudentsFollowing;
+CREATE VIEW StudentsFollowing AS
+	SELECT Students.NIN, Students.name, Students.programme, ChosenBranch.branch FROM Students JOIN ChosenBranch ON Students.NIN = ChosenBranch.student;
+
+-- FinishedCourses
+-- For all students, all finished courses, along with their names, grades (grade 'U', '3', '4' or '5') and number of credits.
+DROP VIEW IF EXISTS FinishedCourses;
+CREATE VIEW FinishedCourses AS
+	SELECT Students.name AS Student, Finished.grade, Courses.name AS Course, Courses.credits 
+		FROM Students JOIN Finished ON Students.NIN = Finished.student 
+			JOIN Courses ON Finished.course = Courses.code;
+
+-- Registrations
+-- All registered and waiting students for all courses, along with their waiting status ('registered' or 'waiting').
+DROP VIEW IF EXISTS Registrations;
+CREATE VIEW Registrations AS
+	SELECT Students.name AS Student, C.course AS Course,
+			CASE WHEN C.course IN (SELECT course FROM Registered) THEN 'Registered' ELSE 'WaitingOn' END AS Status
+		FROM Students NATURAL JOIN
+			((SELECT * FROM Students JOIN Registered ON Students.NIN = Registered.student) AS A
+						NATURAL FULL JOIN (SELECT * FROM Students JOIN WaitingOn ON Students.NIN = WaitingOn.student) AS B) AS C;
+			
+-- PassedCourses
+-- For all students, all passed courses, i.e. courses finished with a 
+-- grade other than ‘U’, and the number of credits for those courses. 
+-- This view is intended as a helper view towards the PathToGraduation
+-- view (and for task 4), and will not be directly used by your application.
+DROP VIEW IF EXISTS PassedCourses CASCADE;
+CREATE VIEW PassedCourses AS
+	SELECT NIN, course, grade, credits FROM
+		Courses JOIN
+			(Students JOIN Finished 
+				ON Students.NIN = Finished.student AND Finished.grade IN ('3','4','5')) 
+			AS A
+		ON Courses.code = A.course;
+
+-- UnreadMandatory
+-- For all students, the mandatory courses (branch and programme) they 
+-- have not yet passed. This view is intended as a helper view towards 
+-- the PathToGraduation view, and will not be directly used by your application.
+DROP VIEW IF EXISTS UnreadMandatory;
+CREATE VIEW UnreadMandatory AS
+	SELECT Students.NIN, A.course FROM
+		(SELECT programme,course FROM ProgrammeMandatory UNION SELECT programme,course from BranchMandatory) AS A
+	JOIN
+		Students ON A.programme = Students.programme AND 
+			(Students.NIN,A.course) NOT IN (SELECT NIN,course FROM PassedCourses)
+	ORDER BY NIN;
+
+-- PathToGraduation
+-- For all students, their path to graduation, i.e. a view with columns for
+-- - the number of credits they have taken.
+-- - the number of mandatory courses they have yet to read (branch or programme).
+-- - the number of credits they have taken in courses that are classified as math courses.
+-- - the number of credits they have taken in courses that are classified as research courses.
+-- - the number of seminar courses they have read.
+-- whether or not they qualify for graduation.
+DROP VIEW IF EXISTS PathToGraduation;
+CREATE VIEW PathToGraduation AS
+	SELECT 
+		Passed.NIN, 
+		CollectedCredits, 
+		CASE WHEN UnreadCourses IS NULL THEN 0 ELSE UnreadCourses END,
+		CASE WHEN MathCredits IS NULL THEN 0 ELSE MathCredits END,
+		CASE WHEN ResearchCredits IS NULL THEN 0 ELSE ResearchCredits END,
+		CASE WHEN ReadSeminarCourses IS NULL THEN 0 ELSE ReadSeminarCourses END
+	FROM
+	(SELECT NIN,SUM(credits) AS CollectedCredits 
+		FROM PassedCourses 
+		GROUP BY NIN) AS Passed
+	LEFT OUTER JOIN
+	(SELECT NIN,COUNT(course) AS UnreadCourses
+		FROM UnreadMandatory
+		GROUP BY NIN) AS Unread
+	ON Passed.NIN = Unread.NIN
+	LEFT OUTER JOIN
+	(SELECT NIN,SUM(credits) AS MathCredits
+		FROM HasClass JOIN PassedCourses
+		ON HasClass.course = PassedCourses.course AND HasClass.class = 'Math'
+		GROUP BY NIN) AS PassedMath
+	ON Passed.NIN = PassedMath.NIN
+	LEFT OUTER JOIN
+	(SELECT NIN,SUM(credits) AS ResearchCredits
+		FROM HasClass JOIN PassedCourses
+		ON HasClass.course = PassedCourses.course AND HasClass.class = 'Research'
+		GROUP BY NIN) AS PassedResearch
+	ON Passed.NIN = PassedResearch.NIN
+	LEFT OUTER JOIN
+	(SELECT NIN,COUNT(credits) AS ReadSeminarCourses
+		FROM HasClass JOIN PassedCourses
+		ON HasClass.course = PassedCourses.course AND HasClass.class = 'Seminar'
+		GROUP BY NIN) AS ReadSeminar
+	ON Passed.NIN = ReadSeminar.NIN;
+
+			--AND (HasClass.class = 'Math' OR HasClass.class = 'Research')
+		/*(SELECT Students.NIN, credits FROM
+			Students JOIN PassedCourses ON Students.NIN = PassedCourses.NIN) AS Passed*/
+		/*JOIN
+		(SELECT Students.NIN, course FROM
+			Students JOIN UnreadMandatory ON Students.NIN = UnreadMandatory.NIN) AS NotPassed
+		ON Passed.NIN = NotPassed.NIN*/
